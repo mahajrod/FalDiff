2b3db6bbb191fefa02638a587c1ea8f2f517fc36
Ilia <grayfall9@me.com>
2015-07-04 20:53:19 +0300

    Finished a sketch of ConnectedComponent

--- datasimulator.py
+++ datasimulator.py
@@ -15,13 +15,16 @@ class ConnectedComponent:
     """
     Basic components of an AbsoluteSample graph.
     """
-    def __init__(self, means, sds, corr_sd=0.2):
+    def __init__(self, means, sds, component_n, corr_sd=0.2):
         if len(means) != len(sds):
-            raise ValueError("Unmatched number of means and standard deviations")
+            raise ValueError("Unmatched numbers of means and standard deviations")
+        self._component_n = component_n
 
         self._means = np.array(means)
         self._sds = np.array(sds)
         self._size = len(means)
+
+        self._names = ["{}{}".format(component_n, i) for i in range(len(self))]
 
         self._corr_matrix = self._gen_correlation_matrix(corr_sd)
         self._corr_matrix_root = np.linalg.cholesky(self._corr_matrix)
@@ -37,16 +40,42 @@ class ConnectedComponent:
                                                               loc=corr_mean, scale=corr_sd,
                                                               size=len(self)))
         corr_matrix += np.identity(len(self))
-
         return corr_matrix
 
-    def take_sample(self, n, size=500):
+    def _sample_from_distributions(self, means, sds, n, size):
         indep_samples = np.vstack([np.ceil(stats.truncnorm.rvs(a=0, b=(5 + mean/sd),
                                                                loc=mean, scale=sd, size=size))
-                                   for sd, mean in zip(self._means, self._sds)])
+                                   for sd, mean in zip(means, sds)])
 
         dep_samples = self._corr_matrix_root.dot(indep_samples)
         return dep_samples[:, random.sample(range(size), n)], np.corrcoef(dep_samples)
+
+    def sample(self, n, size=500):
+        return self._sample_from_distributions(self._means, self._sds, n, size)
+
+    def sample_mutated(self, factors, n, size=500):
+        if len(factors) > len(self):
+            raise ValueError("More factors than components to mutate")
+        if not all([0 <= x <= 1 for x in factors]):
+            raise ValueError("All factors must be real numbers in [0, 1]")
+        iter_factors = iter(factors)
+
+        elements_to_mutate = random.sample(range(len(self)), len(factors))
+        mutator_vector = np.fromiter((next(iter_factors) if i in elements_to_mutate else 1
+                                      for i in range(len(self))), dtype=np.float64)
+        mut_means = np.copy(self._means) * mutator_vector
+        mut_sds = np.copy(self._sds) * mutator_vector
+
+        return self._sample_from_distributions(mut_means, mut_sds, n, size), elements_to_mutate
+
+    @property
+    def names(self):
+        return self._names
+
+
+class AbsoluteSample:
+    def __init__(self, size=100, poisson_lam=1):
+        pass
 
 
 def draw_means_from_normal(mean, sd, size):
@@ -65,9 +94,3 @@ def cv_to_sd(means, sds):
     return means * sds
 
 
-
-
-
-class AbsoluteSample(object):
-    def __init__(self, size=100, poisson_lam=1):
-        pass
